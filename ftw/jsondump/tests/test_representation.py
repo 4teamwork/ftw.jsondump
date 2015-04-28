062e52b54c494f9d427bce79ab10e3389cc6e8f5
Jonas Baumann <jone@jone.ch>
2015-04-28 10:51:46 +0200

    refactor representation test to actually test the JSON content.

--- ftw/jsondump/tests/test_representation.py
+++ ftw/jsondump/tests/test_representation.py
@@ -1,9 +1,11 @@
 from DateTime import DateTime
+from datetime import datetime
 from ftw.builder import Builder
 from ftw.builder import create
 from ftw.jsondump.interfaces import IJSONRepresentation
 from ftw.jsondump.testing import FTW_JSONDUMP_INTEGRATION_TESTING
 from ftw.jsondump.tests.helpers import asset
+from ftw.testing import freeze
 from StringIO import StringIO
 from unittest2 import TestCase
 from zope.component import getMultiAdapter
@@ -14,43 +16,36 @@ class TestJSONRepresentation(TestCase):
 
     layer = FTW_JSONDUMP_INTEGRATION_TESTING
 
-    def setUp(self):
-        ref_document1 = create(Builder('document')
-                               .titled("Ref 1"))
-        ref_document2 = create(Builder('document')
-                               .titled("Ref 2"))
-
-        file_data = 'File data'
-        file_ = StringIO(file_data)
+    def test_archetypes_document(self):
+        file_ = StringIO('File data')
         file_.filename = 'test.doc'
 
         image_ = StringIO(asset('empty.gif').bytes())
         image_.filename = 'test.gif'
 
-        self.document = create(Builder('document')
-                               .titled("My document")
-                               .having(text='<p>"S\xc3\xb6mesimple" <b>markup</b></p>',
-                                       effectiveDate=DateTime(),
-                                       demo_interger_field=42,
-                                       demo_float_field=42.0,
-                                       demo_fixedpoint_field="42.00",
-                                       relatedItems=[ref_document1, ref_document2],
-                                       demo_file_blob_field=file_,
-                                       demo_image_blob_field=image_))
+        with freeze(datetime(2010, 12, 28, 10, 55, 12)):
+            document = create(
+                Builder('document')
+                .titled("My document")
+                .having(text='<p>"S\xc3\xb6mesimple" <b>markup</b></p>',
+                        effectiveDate=DateTime(),
+                        demo_interger_field=42,
+                        demo_float_field=42.0,
+                        demo_fixedpoint_field="42.00",
+                        relatedItems=[create(Builder('document').titled("Ref 1")),
+                                      create(Builder('document').titled("Ref 2"))],
+                        demo_file_blob_field=file_,
+                        demo_image_blob_field=image_))
 
-    def test_data_is_json_serializable(self):
-        adapter = getMultiAdapter((self.document, self.document.REQUEST),
-                                  IJSONRepresentation)
+        adapter = getMultiAdapter((document, document.REQUEST), IJSONRepresentation)
+        data = json.loads(adapter.json())
+        data['relatedItems:uuid'] = ['<REPLACED>' for uid in data['relatedItems:uuid']]
 
-        json_data = adapter.json()
-        self.assertTrue(isinstance(json.loads(json_data), dict))
+        expected = json.loads(asset('archetypes_document.json').text())
+        self.assert_structure_equal(expected, data)
 
-    def test_localroles_data_is_always_present(self):
-        adapter = getMultiAdapter((self.document, self.document.REQUEST),
-                                  IJSONRepresentation)
-
-        json_data = adapter.json()
-
-        self.assertIn('__ac_local_roles__',
-                      json_data,
-                      '__ac_local_roles__ is not present.')
+    def assert_structure_equal(self, expected, got, msg=None):
+        got = json.dumps(got, sort_keys=True, indent=4)
+        expected = json.dumps(expected, sort_keys=True, indent=4)
+        self.maxDiff = None
+        self.assertMultiLineEqual(got, expected, msg)
