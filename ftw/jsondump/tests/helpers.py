7402ed727b5c6cf383ad5fd769d43b6e861d3b0c
Jonas Baumann <jone@jone.ch>
2015-04-29 11:48:36 +0200

    Add file_callback to file extractors.
    The ``file_callback`` setting can be set to a callable, which is called for each
    extracted file.  It allows to handle files differently when the user does not want
    to include them in the JSON for example.
    
    file_callback signature:
    ``file_callback(context, fieldname, data, filename, mimetype, jsondata)``
    
    - ``context``: The extracted Plone object.
    - ``fieldname``: The extracted fields' name.
    - ``data``: The raw file data.
    - ``filename``: The filename of the extracted file.
    - ``mimetype``: The mimetype of the extracted file.
    - ``jsondata``: A pointer to the partial dict which is merged into the JSON later.

--- ftw/jsondump/tests/helpers.py
+++ ftw/jsondump/tests/helpers.py
@@ -1,5 +1,13 @@
 from path import Path
+from StringIO import StringIO
+
 
 def asset(filename):
     path = Path(__file__).dirname().realpath().joinpath('assets', filename)
     return path
+
+
+def asset_as_StringIO(filename):
+    result = StringIO(asset(filename).bytes())
+    result.filename = filename
+    return result
