0aaf2b5703f1d40eedf314f35af7473a4f626c47
Jonas Baumann <jone@jone.ch>
2015-05-05 09:53:29 +0200

    reindent setup.py.

--- setup.py
+++ setup.py
@@ -18,17 +18,17 @@ setup(name='ftw.jsondump',
       version=version,
       description="JSON representation for plone content",
       long_description=open("README.rst").read() + "\n" + open(
-          os.path.join("docs", "HISTORY.txt")).read(),
+        os.path.join("docs", "HISTORY.txt")).read(),
 
       classifiers=[
-          "Environment :: Web Environment",
-          'Framework :: Plone',
-          'Framework :: Plone :: 4.3',
-          "Intended Audience :: Developers",
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
+        "Environment :: Web Environment",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
 
       keywords='ftw json representation serialize dump',
       author='4teamwork GmbH',
@@ -42,10 +42,9 @@ setup(name='ftw.jsondump',
       zip_safe=False,
 
       install_requires=[
-          'setuptools',
-          'Products.CMFCore',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'Products.CMFCore',
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
 
